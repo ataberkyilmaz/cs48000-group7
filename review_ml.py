cc6a3489e8e6e0b41ac3bc8048dab0517afa2913
ataberkyilmaz <ataberkyilmaz@sabanciuniv.edu>
2022-05-12 21:23:00 +0300

    ml complete

--- review_ml.py
+++ review_ml.py
@@ -3,6 +3,7 @@ import os.path
 import csv
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
 
 save_path = r"C:\Users\atabe\GitRepos\cs48000-group7"
 
@@ -30,7 +31,7 @@ if __name__ == '__main__':
     labels = df['sentiment'].values
     reviews_train, reviews_test, y_train, y_test = train_test_split(reviews, labels, test_size=0.2, random_state=1000)
 
-    vectorizer = CountVectorizer(tokenizer = spacy_tokenizer, ngram_range=(1,1))
+    vectorizer = CountVectorizer()
     #By default, the vectorizer might be created as follows:
     #vectorizer = CountVectorizer()
     vectorizer.fit(reviews_train)
@@ -38,8 +39,14 @@ if __name__ == '__main__':
     X_train = vectorizer.transform(reviews_train)
     X_test = vectorizer.transform(reviews_test)
 
-    print(X_train)
-    print(X_test)
+    classifier = LogisticRegression()
+    classifier.fit(X_train, y_train)
 
-    
+    accuracy = classifier.score(X_test, y_test)
+    print("Accuracy:", accuracy)
+
+    # new_reviews = ['Old version of python useless', 'Very good effort, but not five stars', 'Clear and concise']
+    # X_new = vectorizer.transform(new_reviews)
+    # print(classifier.predict(X_new))
+
     print("----------------DONE------------------------")
