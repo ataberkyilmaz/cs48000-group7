5299b532f3702a54f351d6a8ac0fdc1b7af66aff
UtkanKarahan <102539382+UtkanKarahan@users.noreply.github.com>
2022-05-12 21:27:39 +0300

    ml

--- review_ml.py
+++ review_ml.py
@@ -5,24 +5,19 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 
-save_path = r"C:\Users\atabe\GitRepos\cs48000-group7"
 
-completeName = os.path.join(save_path, "sorted_comments.txt") 
-completeNameCsv = os.path.join(save_path, "sorted_comments.csv") 
+def predictData(comment_list):
+    save_path = r"C:\Users\karah\Desktop\cs48000-group7"
 
-
-
-if __name__ == '__main__':
+    completeName = os.path.join(save_path, "sorted_comments.txt") 
+    completeNameCsv = os.path.join(save_path, "sorted_comments.csv") 
 
     with open(completeName, 'r') as in_file:
         stripped = (line.strip() for line in in_file)
         lines = (line.split(",") for line in stripped if line)
         with open(completeNameCsv, 'w') as out_file:
             writer = csv.writer(out_file)
-            writer.writerows(lines)
-
-    # read_file = pd.read_csv(completeName)
-    # read_file.to_csv(completeNameCsv, index=None)    
+            writer.writerows(lines)  
 
     df = pd.read_csv(completeNameCsv, names=['review', 'sentiment'], sep=',') 
     print(df.head())
@@ -32,8 +27,6 @@ if __name__ == '__main__':
     reviews_train, reviews_test, y_train, y_test = train_test_split(reviews, labels, test_size=0.2, random_state=1000)
 
     vectorizer = CountVectorizer()
-    #By default, the vectorizer might be created as follows:
-    #vectorizer = CountVectorizer()
     vectorizer.fit(reviews_train)
 
     X_train = vectorizer.transform(reviews_train)
@@ -45,8 +38,15 @@ if __name__ == '__main__':
     accuracy = classifier.score(X_test, y_test)
     print("Accuracy:", accuracy)
 
-    # new_reviews = ['Old version of python useless', 'Very good effort, but not five stars', 'Clear and concise']
-    # X_new = vectorizer.transform(new_reviews)
-    # print(classifier.predict(X_new))
+    
+    X_new = vectorizer.transform(comment_list)
+    return classifier.predict(X_new)
+
+
+
+if __name__ == '__main__':
+
+    new_reviews = ['Old version of python useless', 'Very good effort, but not five stars', 'Clear and concise']
+    print(predictData(new_reviews))
 
     print("----------------DONE------------------------")
